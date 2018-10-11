4bc344f53d8bb4348276f6f6805776aaa03c6ce9
GAVEAU Francois-Axel <gaveau.francois-axel@orange.fr>
2018-10-11 13:12:38 +0200

    Create quest react router

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Route, BrowserRouter, Switch, NavLink } from "react-router-dom";
+import Home from "./Home";
+import History from "./History";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+          <NavLink to="/notre-histoire" activeStyle={{ color: "red" }}>
+            {" "}
+            History{" "}
+          </NavLink>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/notre-histoire" component={History} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p>Bienvenue sur l'accueil</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
